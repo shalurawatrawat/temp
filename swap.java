4878594264bfd6193534d52fd18b05536c7f4e1a
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-04 10:40:29 +0530

    first commit

---
+++ swap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Demo
+{
+    public static void main(String uu[])
+    {
+        int a,b,c;
+        Scanner ob=new Scanner(System.in);
+        System.out.print("befour swapping")
+       System.out.print("enter a no");
+       a=ob.nextInt();
+       System.out.print("enter 2second no");
+       b=ob.nextInt();
+       
+       c=a;
+       a=b;
+       b=c;
+       System.out.println("After swapping");
+      System.out.println("valu of a" +a);
+      System.out.println("value of b" +b);
+      
+        
+    }
+}
