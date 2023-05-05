af216ab9a6235d37290e4e4af35875541a9a20db
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-05 11:06:06 +0530

    hello

--- hello.java
+++ hello.java
@@ -1,26 +1,8 @@
-import java.util.*;
-class Demo
+
+class Hello
 {
-    public static void main(String uu[])
-    {
-        int a,b,c;
-        Scanner ob=new Scanner(System.in);
-       System.out.printf("enter a no");
-       a=ob.nextInt();
-       System.out.print("enter 2second no"
-       b=ob.nextInt();
-       System.out.print("enter 3thred no");
-       c=ob.nextInt();
-       System.out.println("before Swapping");
-       System.out println("a"+a);
-       System.out println("b"+b);
-       System.out println("c"+c);
-       a=b+c;
-       b=a-b;
-       a=a-b;
-       System .out.println ("After Swapping");
-       System .out.println("a"+a);
-        System .out.println("b"+b);
-         System .out.println("c"+a);
-    }
+public static void main(String a[])
+{
+System.out.println("hello");
+}
 }
