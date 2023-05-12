50731a1d963d5c757037f96319d2d6917af06d57
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-12 10:13:32 +0530

    Easy

---
+++ Easy.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Easy
+{
+    public static void main(string arr[])
+    {
+        int a;
+        System.out.println("enter a no");
+        a=ob.nextInt();
+        switch(a) 
+        {
+            case 1:cmd
+                System .out.println("sunday");
+                break;
+                case 2:
+                System.out.println("monday");
+                break;
+                case 3:
+                System .out.println("tuesday");
+                break;
+                case 4:
+                System .out.println("Wednesday");
+                break;
+                case 5:
+                System .out.println("Thursday");
+                break;
+                case 6:
+                System .out.println(" Friday");
+                break;
+                case 7:
+                System .out.println(" Saturday");
+                break;
+            default:
+                break;
+        }
+    }
+}
