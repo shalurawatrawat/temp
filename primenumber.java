f2c50a740e0824c1289193c1a2d944e06392eaab
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-18 21:33:43 +0530

    prime no

---
+++ primenumber.java
@@ -0,0 +1,21 @@
+import java .util.Scanner;
+class Factoyal
+{
+    public static void main(String arr[])
+    {
+        int n,i;
+        Scanner ob=new  Scanner(System.in);
+        System.out.println("enter a no"); 
+        n=ob.nextInt();
+       
+
+if(n%2==1)
+{
+System.out.println("prime");
+}else
+{
+System.out.println("not prime");
+}
+        
+}
+}
