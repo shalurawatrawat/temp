fc09ec56aa4bef98afeeb4a96dfd6b55cd21ee01
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-18 11:48:18 +0530

    Factoyal

---
+++ Factoria.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Factoria
+{
+  public static void main(String arr[])
+  {
+    int i,n,fact=1;
+    Scanner ob = new Scanner(System.in);
+  
+      System.out.print("Enter a no..");
+      n = ob.nextInt();
+      for(i=1;i<=n;i++){
+      fact *= i;
+    }
+     System.out.print(fact);
+}
+  }
+
+
