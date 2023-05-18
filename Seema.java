7057a77183daac934dff388da59ec0e04e99b921
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-18 21:28:37 +0530

    fess colculate

---
+++ Seema.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+class Demo
+{
+    public static void main(String arr[])
+    {
+        int c,m,choose,fess=0;
+        Scanner ob.new Scanner(System.in);
+        System.out.println("ente a class");
+c=ob.nextInt();
+System.out.println("chose your mmnth");
+c=ob.nextInt();
+Swatich(c)
+{
+    case1:
+    fess=m*100;
+    break;
+    case2:
+    fess=m*200;
+    break;
+    case3:
+    fess=m*300;
+    break;
+    case4:
+    fess=m*400;
+    break;
+    case5:
+    fess=m*500;
+    break;
+    case6:
+    fess=m*600;
+    break;
+    case7:
+    fess=m*600;
+    break;
+    case8:
+    fess=m*600;
+    break;
+    case9:
+    fess=m*600;
+    break;
+    case10:
+    fess=m*600;
+    break;
+    case11:
+    fess=m*600;
+    break;
+    case12:
+    fess=m*600;
+    break;
+    {
+        System.out.println("totle fesss"+fess);
+    }
+}
+    }
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+
