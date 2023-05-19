21b6593920468022f116dea12705e75ba488f1aa
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-19 11:32:33 +0530

    equalarry

---
+++ equalarry.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+public static void main (String[] args) 
+{
+   String st1="ajmer";
+   String st2 ="ajmera";
+   String st3 ="ajmer";
+   System.out.println(st1.equals (st2));
+   System.out.println(st1.equals (st3));
+}
+}
