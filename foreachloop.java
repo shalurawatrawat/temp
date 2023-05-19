043217472c3982d68ca3b32d6210796a43c682f9
shalurawatrawat <shalusoftdev@gmail.com>
2023-05-19 10:31:52 +0530

    foreachloop

---
+++ foreachloop.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Renu
+{
+	public static void main(String[] args) 
+	{ 
+	    String st[] = {"renu", "sonu" ,"shalu"};
+	    int i[]={2,3,4,5,6,7};
+	    for(int k:i)
+	    {
+		System.out.println(k +" ");
+	    }
+	     for(String z:st)
+	     {
+	         System.out.print(z +" ");
+	     }
+	}
+}
+
